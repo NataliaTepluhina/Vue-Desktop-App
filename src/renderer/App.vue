65808a4d70989f756caecdb17cfd55dd2100bde2
NataliaTepluhina <tarya.se@gmail.com>
2018-09-09 21:28:52 +0300

    * fixed axios in App.vue component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -27,8 +27,6 @@
 </template>
 
 <script>
-import axios from 'axios';
-axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
 const API_KEY = 'cf13c71a52bad00d4b2c1f253e198b06';
 export default {
   name: 'weather-app',
@@ -48,8 +46,12 @@ export default {
   },
   methods: {
     showWeather() {
-      axios
-        .get(`/weather?q=${this.query}&units=metric&&appid=${API_KEY}`)
+      this.$http
+        .get(
+          `http://api.openweathermap.org/data/2.5/weather?q=${
+            this.query
+          }&units=metric&&appid=${API_KEY}`,
+        )
         .then(response => {
           this.city = response.data.name;
           this.country = response.data.sys.country;

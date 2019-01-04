3de597f77408efa38dbf37d5189c706d1a0c3b6d
Natalia Tepluhina <tarya.se@gmail.com>
2019-01-04 17:41:48 +0200

    Finish web app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 
+axios.defaults.baseURL = 'http://api.openweathermap.org/data/2.5';
+
 Vue.config.productionTip = false
+Vue.http = Vue.prototype.$http = axios;
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
